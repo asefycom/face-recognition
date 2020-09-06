ab6e508161b28f3ced949661840ee9f894c42e73
mostafa-asefy <asefi.v@gmail.com>
2020-09-06 11:17:28 +0430

    L05: face locations

--- study.py
+++ study.py
@@ -9,6 +9,12 @@ imgShajarian01 = face_recognition.load_image_file(shajarian01_file)
 # print(imgShajarian01)
 # print('-----------')
 # print(imgShajarian01CV)
+shajarian01_face_loc = face_recognition.face_locations(imgShajarian01)[0]
+print(shajarian01_face_loc)
+
+face_rec = cv2.rectangle(imgShajarian01, (shajarian01_face_loc[3],shajarian01_face_loc[0]),
+                         (shajarian01_face_loc[1], shajarian01_face_loc[2]),(255,0,255),3)
+
 
 imgShajarian01 = cv2.cvtColor(imgShajarian01,cv2.COLOR_RGB2BGR)
 cv2.imshow('shajarian basic image', imgShajarian01)
