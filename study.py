dd8348c3d933683eef441adb86bf36ef3f8834af
mostafa-asefy <asefi.v@gmail.com>
2020-09-06 11:24:33 +0430

    L05-Practice: Two Faces-TwoImages

--- study.py
+++ study.py
@@ -1,21 +1,25 @@
 import cv2
 import face_recognition
 
-shajarian01_file = 'images/shajarian01.jpg'
 
+shajarian01_file = 'images/shajarian01.jpg'
 imgShajarian01 = face_recognition.load_image_file(shajarian01_file)
-# imgShajarian01CV = cv2.imread(shajarian01_file)
-#
-# print(imgShajarian01)
-# print('-----------')
-# print(imgShajarian01CV)
 shajarian01_face_loc = face_recognition.face_locations(imgShajarian01)[0]
 print(shajarian01_face_loc)
+face01_rec = cv2.rectangle(imgShajarian01, (shajarian01_face_loc[3],shajarian01_face_loc[0]),
+                           (shajarian01_face_loc[1], shajarian01_face_loc[2]),(255,0,255),3)
 
-face_rec = cv2.rectangle(imgShajarian01, (shajarian01_face_loc[3],shajarian01_face_loc[0]),
-                         (shajarian01_face_loc[1], shajarian01_face_loc[2]),(255,0,255),3)
+
+shajarian02_file = 'images/shajarian02.jpg'
+imgShajarian02 = face_recognition.load_image_file(shajarian02_file)
+shajarian02_face_loc = face_recognition.face_locations(imgShajarian02)[0]
+print(shajarian02_face_loc)
+face02_rec = cv2.rectangle(imgShajarian02, (shajarian02_face_loc[3],shajarian02_face_loc[0]),
+                           (shajarian02_face_loc[1], shajarian02_face_loc[2]),(255,0,255),3)
 
 
 imgShajarian01 = cv2.cvtColor(imgShajarian01,cv2.COLOR_RGB2BGR)
-cv2.imshow('shajarian basic image', imgShajarian01)
+imgShajarian02 = cv2.cvtColor(imgShajarian02,cv2.COLOR_RGB2BGR)
+cv2.imshow('shajarian training image', imgShajarian01)
+cv2.imshow('shajarian test image', imgShajarian02)
 cv2.waitKey(0)
